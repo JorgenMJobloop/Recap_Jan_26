3c013e4f48918dc3cded42dd707f45ec6e5a91db
JorgenMJobloop <jorgen@jobloop.no>
2026-01-05 10:56:11 +0100

    added interface example

---
+++ Classes/Calculator.cs
@@ -0,0 +1,26 @@
+public class Calculator : ICalculator
+{
+    public double AddNumbers(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double DivideNumbers(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException();
+        }
+        return a / b;
+    }
+
+    public double MultiplyNumbers(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double SubtractNumbers(double a, double b)
+    {
+        return a - b;
+    }
+}
---
+++ Interface/ICalculator.cs
@@ -0,0 +1,7 @@
+public interface ICalculator
+{
+    double AddNumbers(double a, double b);
+    double SubtractNumbers(double a, double b);
+    double MultiplyNumbers(double a, double b);
+    double DivideNumbers(double a, double b);
+}
